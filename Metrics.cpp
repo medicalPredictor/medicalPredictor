21ddc28b7f8908de2ca0b3729f5f96283a7897de
matthewk2045@hotmail.ca <matthewk2045@hotmail.ca>
2022-10-12 15:28:59 -0400

    metric adding

--- Metrics.cpp
+++ Metrics.cpp
@@ -2,10 +2,30 @@
 
 /**
  * Fitness functions for our evolver.
+ * INPUT:
+ * intData -> data from the M.P
+ * timeScale -> averaging done by real world data at each timestep
+ * typeAgg -> type of averaging: 0 == left weighted, 1 == centered weighted, 2 == right weighted
+ * OUTPUT: 
+ * aggData -> data averaged based upon real world timescale
  */
-float ** aggregateData(vector<float> intData ) {
-    aggData = 
-    aggTimeScale = vector<float>
+float ** aggregateData(vector<float> intData, vector<float> timeScale, int typeAgg) {
+    vector<float> aggData{intData};
+    for (int i=0; i<size(aggData); i++){
+        aggData[i] = 0
+        for( int j=0; j<timeScale[i]; j++){
+            if typeAgg == 0 {
+            // Force it to spit out 0 if not enough data points
+                if i-j <0{
+                    sum = 0
+                    break;
+                } else {
+                    sum += intData[i-j];
+                }
+            }
+        }
+    }
+
     return (0);
 }
 
