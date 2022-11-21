600d7c37430b9661557b597b70e4e24d9efbc2fe
Michael Dubé <michael.dube.778@gmail.com>
2022-11-21 15:51:01 -0500

    Added variance and aggregator methods to Metrics.cpp.

--- Metrics.cpp
+++ Metrics.cpp
@@ -1,44 +1,89 @@
-#include "Graph.h"
+#include <cmath>
+#include <vector>
+#include <iostream>
+
+#define verbose
+
+using namespace std;
 
 /**
- * Scales our data to realworld data
- * INPUT:
- * intData -> data from the M.P
- * timeScale -> averaging done by real world data at each timestep
- * typeAgg -> type of averaging: 0 == left weighted, 1 == centered weighted, 2 == right weighted
- * OUTPUT: 
- * aggData -> data averaged based upon real world timescale
+ * Takes real data and simulated data, outputs variance (RMS)
+ *
+ * @param realData -> data from real world location
+ * @param simData -> data made from M.P
+ * @return variance -> RMS variance
  */
-float ** aggregateData(vector<float> intData, vector<float> timeScale, int typeAgg) {
-    vector<float> aggData{intData};
-    for (int i=0; i<size(aggData); i++){
-        aggData[i] = 0
-        for( int j=0; j<timeScale[i]; j++){
-            if typeAgg == 0 {
-            // Force it to spit out 0 if not enough data points
-                if i-j <0{
-                    sum = 0
-                    break;
-                } else {
-                    sum += intData[i-j];
-                }
-            }
-        }
+double vars(vector<float> realData, vector<float> simData) {
+    double variance = 0;
+    // calculates variance :^)
+    if (realData.size() != simData.size()){
+        cout<<"ERROR in method 'vars': realData and simData different sizes."<<endl;
+        return (-1);
     }
 
-    return (0);
+    for (int i = 0; i < realData.size(); ++i) {
+        variance += pow(realData.at(i)-simData.at(i),2);
+    }
+
+    variance /= (double)realData.size();
+    variance = sqrt(variance);
+
+    return variance;
 }
 
-/**
-* Takes real data and simulated data, outputs difference in points squared
-* INPUT:
-* realData -> data from real world location
-* simData -> data made from M.P
-* OUTPUT:
-* variance -> summed difference squared
- */
-int vars(vector<float> realData, vector<float> simData) {
-    variance = 0;
-    // calculates variance :^)
-    return (variance);
+vector<vector<float > > aggr(vector<float> ogData, int binSize, bool keepEnds){
+    vector<vector<float>> results;
+    vector<float> bins;
+    float binSum = 0.0;
+    int numBins;
+
+    if (ogData.size() < binSize){
+        cout<<"ERROR in method 'aggr': binSize larger than ogData size."<<endl;
+        return results;
+    }
+
+    for (int bs = 0; bs < binSize; ++bs) {
+        numBins = (int) (ogData.size() - bs) / binSize;
+        binSum=0;
+        if (keepEnds && bs != 0) {
+            for (int d = 0; d < bs; ++d) {
+                binSum += ogData[d];
+            }
+            bins.push_back(binSum);
+        }
+        for (int b = 0; b < numBins; ++b) {
+            binSum = 0.0;
+            for (int d = bs + b * binSize; d < bs + (b + 1) * binSize; ++d) {
+                binSum += ogData[d];
+            }
+            bins.push_back(binSum);
+        }
+        if (keepEnds && (ogData.size() - bs) % binSize != 0) {
+            binSum = 0;
+            for (int i = bs + numBins * binSize; i < ogData.size(); ++i) {
+                binSum += ogData[i];
+            }
+            bins.push_back(binSum);
+        }
+        results.push_back(bins);
+        bins.clear();
+    }
+    return results;
+}
+
+int main(){
+    vector<float> v1 = {1,2,3};
+    vector<float> v2 = {3,4,7};
+    cout<<vars(v1,v2)<<endl;
+
+    vector<float> v3 = {20, 1, 1, 1, 2, 2, 2, 3, 3, 3,4,4};
+    vector<vector<float > > lester = aggr(v3, 20, true);
+
+    for (int i = 0; i < lester.size(); ++i) {
+        for (int j = 0; j < lester[i].size(); ++j) {
+            cout<<lester[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
 }
