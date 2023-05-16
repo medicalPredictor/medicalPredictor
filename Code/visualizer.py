7e408aa6635eff147951ba2d69043a0099d88a18
matthewk2045@hotmail.ca <matthewk2045@hotmail.ca>
2023-05-16 13:49:45 -0400

    Good data.dat

--- Code/visualizer.py
+++ Code/visualizer.py
@@ -3,21 +3,29 @@ from PIL import ImageTk, Image
 import matplotlib.pyplot as plt
 
 
-params = Tk() # I dont actually know what to do with this window 
+params = Tk() # The first window
 
+# Makes a new window with an image in the middle
 averagedSims = Toplevel()
-
 averagedSims.title("Averaged Simulations")
 averagedSims.geometry("1000x800")
 avgImage = Frame(averagedSims,width = 700, height = 600)
 avgImage.pack() # 
 avgImage.place(anchor = 'center', relx=0.5, rely = 0.5)
 
-testData = []
-[testData for x in range(100)]
-plt.plot(testData,testData,'ro')
-plt.savefig("testData.png")
+# Pull from file
+data = open("DATA.DAT",'r') # check data example file to make sure, should be I D R then varients
+contents = data.read()
+# Break into experiments
+# Break into individual stuffs  
+print(contents)
 
+# testData = []
+# [testData for x in range(100)]
+# plt.scatter(testData,testData,'ro')
+# plt.savefig("testData.png")
+
+# Actually draws the graph
 avgGraph = ImageTk.PhotoImage(Image.open("testData.png"))
 avgLabel = Label(avgImage, image = avgGraph)
 avgLabel.pack()
