61d61b17643bf00e3531d64f8d4dcaa996a78114
matth <matthewk2045@hotmail.ca>
2023-06-27 13:45:15 -0400

    adding graph maker

---
+++ Code/GraphGen.h
@@ -0,0 +1,11 @@
+#include "Graph.h"
+// Library to generate interesting adj matricies 
+// The general graph class only requires the top half of the adj matrix  
+
+
+vector<vector<int> > connectedGraph(int x, int y);
+
+//Connected graph maker, simple the adj is just all 1s
+vector<vector<int> > connectedGraph(int numberofNodes) {
+    printf("test")
+}
