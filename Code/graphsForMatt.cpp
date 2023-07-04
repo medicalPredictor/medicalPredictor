89cb1c2f155dd5549a370e795e19799a2d446b04
matth <matthewk2045@hotmail.ca>
2023-07-04 19:12:20 -0400

    some graph making stuff

--- Code/graphsForMatt.cpp
+++ Code/graphsForMatt.cpp
@@ -25,7 +25,7 @@ void printAdj(vector<vector<int> > adjM);
 //Generates a complete graph with n nodes
 vector<vector<int> > completeGraph(int n) {
     vector<vector<int> > connectedAdj;
-    for (int i = n; i > 0; i--) {
+    for (int i = n-1; i > 0; i--) {
         vector<int> node;
         for (int j = 0; j < i; j++)
             node.push_back(1);
@@ -37,9 +37,9 @@ vector<vector<int> > completeGraph(int n) {
 //Generates a path of length n
 vector<vector<int> > pathGraph(int n) {
     vector<vector<int> > pathAdj;
-    for (int i = n; i > 0; i--) {
+    for (int i = n-1; i > 0; i--) {
         vector<int> node;
-        int k = 1; // ensures path, every node i needs its ith +1 index to be 1
+        int k = 0; // ensures path, every node i needs its ith +1 index to be 1
         for (int j = 0; j < i; j++) {
             if (j == k)
                 node.push_back(1);
@@ -56,11 +56,11 @@ vector<vector<int> > randomGraph(int n, double p) {
     vector<vector<int> > randAdj;
     double randNum;
     srand48((int) time(nullptr));
-    for (int i = n; i > 0; i--) {
+    for (int i = n-1; i > 0; i--) {
         vector<int> node;
         for (int j = 0; j < i; j++) {
             randNum = drand48();
-            if (randNum > p)
+            if (randNum < p)
                 node.push_back(1);
             else
                 node.push_back(0);
@@ -73,35 +73,31 @@ vector<vector<int> > randomGraph(int n, double p) {
 //Generates a tree with n members of depth m as best as possible
 vector<vector<int> > tree(int n, int m) {
     vector<vector<int> > treeAdj;
+    int parition = lrand48() % n;
+    while (parition < n) {
+        /* code */
+    }
+    
     return(treeAdj);
 }
 
 
-//Generates a tree with n members, it will have an arbtirary width and depth based upon probability 1 - p
-// FIX
-vector<vector<int> > randomTree(int n, double p) {
+//Generates a tree with n members by randomly partitioning the number of nodes
+vector<vector<int> > randomTree(int n) {
     vector<vector<int> > rTreeAdj;
-    double randNum;
-    bool broken; //breaks the tree level
-    int nextLevelStart = 0; // indiciates where the break occured
     srand48((int) time(nullptr));
-    for (int i = n; i > 0; i--) {
+    int parition = (lrand48() % (n-1)) + 1;
+    int oldPart = 0;
+    for (int i = n-1; i > 0; i--) {
         vector<int> node;
-        broken = true;
-        for (int j = 0; j < nextLevelStart; j++)
-            node.push_back(0);                
-        for (int j = nextLevelStart; j < i; j++) {
-            randNum = drand48();
-            if (randNum > p && broken == false) {
+        for (int j = 0; j < i; j++) {
+            if ((oldPart-1) < j && j <= parition)
                 node.push_back(1);
-                nextLevelStart = j;
-            }
-            else {
+            else
                 node.push_back(0);
-                broken = false;
-            }
         }
-        rTreeAdj.push_back(node);
+        oldPart = parition;
+        parition = (lrand48() % (n-1-parition)) + 1 + parition;
     }
     return(rTreeAdj);
 }
@@ -122,30 +118,38 @@ void printAdj(vector<vector<int> > adjM) {
     }    
 }
 
+void makeBitString(vector<vector<int> > adjM) {
+    for (int i = 0; i < adjM.size(); i++) {
+        for (int j = 0; j < adjM[i].size(); j++)
+            cout << adjM[i][j] << " ";
+        cout << endl;
+    }    
+}
 
 int main() {
-    int size = 4; // general number of nodes
+    int size = 8; // general number of nodes
     double prob = 0.5; // general probability
+    char genericPrint[] = "Strict uppertriangle for";
 
     vector<vector<int> > connected;
     connected= completeGraph(size);
-    printf("Top half of adj matrix for connected graph of size %d \n", size);
+    printf("%s a complete graph with %d nodes\n", genericPrint, size);
     printAdj(connected);
 
     vector<vector<int> > path;
     path = pathGraph(size);
-    printf("Top half of adj matrix for path graph of size %d \n", size);
+    printf("%s a path graph with %d nodes\n", genericPrint, size);
     printAdj(path);
 
     vector<vector<int> > randy;
     randy = randomGraph(size, prob);
-    printf("Top half of adj matrix for random graph of size %d and probability %f \n", size, prob);
+    printf("%s a random graph of size %d and edge probability of %f \n", genericPrint, size, prob);
     printAdj(randy);
 
     // DOES NOT WORK YET
     vector<vector<int> > randyTree;
-    randyTree = randomTree(size, prob);
-    printf("Top half of adj matrix for random tree graph of size %d and probability %f \n", size, prob);
+    randyTree = randomTree(size);
+    printf("%s a random tree graph of size %d \n", genericPrint, size);
     printAdj(randyTree);
 
 
